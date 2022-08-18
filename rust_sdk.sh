3b82bfd3dad373960e33a9f8dae174b688f14f9b
Gabriel Reus <GabrielReusRodriguez@users.noreply.github.com>
2022-08-18 23:36:00 +0200

    Eliminado el echo de pruebas

--- rust_sdk.sh
+++ rust_sdk.sh
@@ -1,5 +1,4 @@
 #/usr/local/bin/bash
-echo "Hola!"
 
 #docker run -it --rm -v --user "$(id -u):$(id -g)"  "$PWD":/usr/src/myapp rust_sdk  $@
 #docker run -it --rm  --user "$(whoami)" -v   "$PWD":/usr/src/myapp rust_sdk  $@
