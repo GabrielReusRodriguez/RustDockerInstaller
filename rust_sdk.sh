ec59b73acdc14bd7da691aec07fc4f168fc64a55
Gabriel Reus <GabrielReusRodriguez@users.noreply.github.com>
2022-08-20 22:09:13 +0200

    Corrección de error en shebang

--- rust_sdk.sh
+++ rust_sdk.sh
@@ -1,4 +1,4 @@
-#/usr/local/bin/bash
+#!/usr/local/bin/bash
 
 #docker run -it --rm -v --user "$(id -u):$(id -g)"  "$PWD":/usr/src/myapp rust_sdk  $@
 #docker run -it --rm  --user "$(whoami)" -v   "$PWD":/usr/src/myapp rust_sdk  $@
